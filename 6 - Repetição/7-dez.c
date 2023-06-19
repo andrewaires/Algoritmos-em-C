c87db63ac94c110cb67f1330026a235f441e67a7
Andrew Aires <andrewairesss@gmail.com>
2023-06-19 17:08:04 -0300

    Add files via upload

---
+++ 6 - Repetição/7-dez.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    int n;
+
+    n = 1;
+    while (n != 11){
+        printf("%d ", n);
+        n += 1;
+    }
+    
+    return 0;
+}
+
+/*9.7.Escreva um algoritmo para imprimir os números de 1 a 10 utilizando uma estrutura ENQUANTO e
+um contador.*/
